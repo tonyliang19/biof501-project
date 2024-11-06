6289f35d8013d4a71400bd0187cbca47b71a192c
tonyliang19 <chunqingliang@gmail.com>
2024-11-05 17:28:35 -0800

    this feature counts works

--- bin/featureCounts.R
+++ bin/featureCounts.R
@@ -1,10 +1,22 @@
+#!/usr/bin/env Rscript
+
 library(Rsubread)
 
-# first parse cli
-cli <- list()
-bam <- cli$bam
-gtf <- cli$gtf
+# first parse cli, this gives as string for every arg
+# So args is just character vector
+args <- commandArgs(trailingOnly = TRUE)
+# BAM could be more than 1 files, these are separated by space
+bam_list <- args[1]
+# The underlying R version is old, so native pipe |> do not support
+bam <- unlist(strsplit(bam_list, split = " "))
+gtf <- args[2]
 
-
-featureCounts(files = bam, annot.ext = gtf, isGTAnnotationFile = TRUE, isPairedEnd = TRUE,
+# Generates count matrix
+fc <- featureCounts(files = bam, annot.ext = gtf, isGTFAnnotationFile = TRUE, isPairedEnd = TRUE,
 GTF.featureType = "gene")
+
+# Lastly write it to file
+file <- "feature_counts.rds"
+saveRDS(fc, file = file)
+
+
